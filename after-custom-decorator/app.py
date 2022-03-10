bb6309516d61eb3cf6550b75c4678e8d8e747254
Jose Salvatierra <jslvtr@gmail.com>
2022-03-10 20:29:09 +0000

    Make project work with flask run by moving db import and db.init_app, and removing the script runtime if statement.

--- after-custom-decorator/app.py
+++ after-custom-decorator/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
+from db import db
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
@@ -12,6 +13,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+db.init_app(app)
 app.secret_key = 'jose'
 api = Api(app)
 
@@ -29,9 +31,3 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(AddDevice, '/user/add-device')
-
-
-if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
-    app.run(port=5000, debug=True)
